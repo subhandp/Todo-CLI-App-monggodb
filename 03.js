b0e95d8fd1c10d44c6b2bddca32694c2e94ecff3
subhandp <subhan.dinda.putra@gmail.com>
2020-07-30 07:03:27 +0800

    add todo update to 03.js

--- 03.js
+++ 03.js
@@ -1,6 +1,29 @@
 const { program } = require("@caporal/core")
 const { sequelize, Todo } = require('./sequelizeModel.js');
 
+const todoUpdate = async(todo, todoId) => {
+
+    const t = await sequelize.transaction();
+    try {
+        const result = await sequelize.transaction(async(t) => {
+
+            await Todo.update({ activity: todo }, {
+                where: {
+                    id: todoId
+                }
+            }, { transaction: t });
+
+        });
+        await t.commit().then((val) => {
+            console.log("Data berhasil di update")
+        });
+    } catch (error) {
+        await t.rollback();
+        console.log('Kesalahan, data gagal di update')
+    }
+
+};
+
 program
 
     .command("update", "Update todo data")
